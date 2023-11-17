5a1dd707bd1b7769cd18c9b269ce93a5384f5976
bitexe <alexbitetto01@gmail.com>
2023-11-17 16:26:42 +0100

    Dockerized application working

--- web-server/src/main.rs
+++ web-server/src/main.rs
@@ -68,7 +68,7 @@ async fn main() -> std::io::Result<()> {
             .route("/submissions", web::get().to(submissions))
             .route("/delete/{id}", web::post().to(delete_submission))
     })
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:8080")?
     .run()
     .await
 }
